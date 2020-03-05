3dfd43a6f8f902b20bccd86d75c71a2bf9c8ca6e
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-03-05 15:08:27 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-       {name: "Benvolino", age: "17", attitude: "hot-headed"},
+       {name: "Benvolio", age: "17", attitude: "hot-headed"},
         {name: "Mercutio", age: "18", attitude: "worried"}
       ]
    }, 
