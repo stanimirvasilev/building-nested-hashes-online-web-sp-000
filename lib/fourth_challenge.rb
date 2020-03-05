ecb6b94f8a79e5dec88c97186269c0cc67acb52a
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-03-05 15:07:08 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -15,7 +15,7 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         {name: "Steven", age: "30", attitude: "confused"},
-        {name: "Mercutio", age: "18", attitude: "worried"}
+        {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
 }
